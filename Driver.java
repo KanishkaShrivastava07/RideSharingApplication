71a492d533949a0bb10bcefcdad0aca72c284bc9
KanishkaShrivastava07 <shrivastavakanishka06@gmail.com>
2026-01-16 19:52:39 +0530

    Create Driver.java

---
+++ Driver.java
@@ -0,0 +1,12 @@
+public class Driver{
+
+    private final int driverId;
+
+    public Driver(int driverId){
+        this.driverId = driverId;
+    }
+
+    public int getDriverId(){
+        return driverId;
+    }
+}
